485e85adb6fb2a811158440bbdf80325d8abea50
ningotputrasijabat <ningotputra2@gmail.com>
2019-11-04 11:44:10 +0700

    Tugas 1 Mobil II

---
+++ gen/com/example/ningotputrasijabat_163303030209_tugas01_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.ningotputrasijabat_163303030209_tugas01_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
